d1401e4adaf7564671ab346158abcab0ed91c5d2
mj roberts <michael.j.roberts33@gmail.com>
2024-07-05 12:00:12 -0500

    commented some updates

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Web_Programming
-An initial repository to build web components and files
+An repository to build web components and files.
+---
+The First components I am working on are a collection of files to show as a customized web page resume.
+I am doing this on my free time and will be continually building onto them. 
 
-need to create an index.html file
-- index.html
+Files built and to be added.
+- [index.html](index.html)
+- [customcss.css](customcss.css)
+- [customjs.js](customjs.js)
---
+++ customjs.js
@@ -0,0 +1,18 @@
+
+// query selector for one first element that matches input
+// let q = document.querySelector();
+
+// query selector for all elements that match the input
+// let q = document.querySelectorAll();
+
+// Getting and Element by its ID, TagName, or Class
+// let q = document.getElementById();
+// let q = document.getElementsByName();
+// let q = document.getElementsByClassName();
+// let q = document.getElementsByTagName();
+
+// If I want to add an event listener to the q variable
+/* q.addEventListener("specificEventListener", function() {
+    alert("example")
+    e.target.input.attribute = "cusomtization";
+});
