64e4901d0b0a743d998e252e4fd488d12e5a48e5
mj roberts <michael.j.roberts33@gmail.com>
2024-07-10 22:47:23 -0500

    1 html and two php files added

---
+++ dataCollectionFromHTML1.php
@@ -0,0 +1,16 @@
+<!-- dataCollectionFromHTML1.php --> 
+<?php
+class Songs {
+    public $songlist = ["song"=>"n1","comp"=>"n2","art"=>"n3"];
+
+    function addSongToList($song, $c, $a) {
+        
+    } 
+    
+
+}
+
+$songName = $_GET["songName"];
+$composer = $_GET["composer"];
+$artist = $_GET["artist"];
+?>
---
+++ surveyResults.php
@@ -0,0 +1,9 @@
+<?php 
+
+for (i = 0; i)
+
+echo "<p>The results from the survey are $"
+
+
+
+?>
