8c75e340cf2429f9f850b97c350201f012a0b5ee
mj roberts <michael.j.roberts33@gmail.com>
2024-07-15 22:01:33 -0500

    added more touches to the Webpage that is supposed to input your favorite songs

--- dataCollectionFromHTML1.php
+++ dataCollectionFromHTML1.php
@@ -1,16 +1,54 @@
 <!-- dataCollectionFromHTML1.php --> 
 <?php
+
+ 
+// $file = fopen($outfile, "(a for append, w for write, r for read"); 
+// if (!$file) {
+//    die("no file matches $outfile");}
+// $outfileContents = fread($file, filesize($outfile));
+// fclose($file);
+
 class Songs {
+    private $outfile = "outfile.txt";
+
     public $songlist = ["song"=>"n1","comp"=>"n2","art"=>"n3"];
 
-    function addSongToList($song, $c, $a) {
-        
+    function addSongToList($song, $co, $ar) {
+        $newSong = [
+            "song" => $song,
+            "composer" => $co,
+            "artist" => $ar
+        ];
+        $currentList = $this->readFromFile();
+        $currentList[] = $newSong;
+        $this->writeToFile($currentList);
+        //foreach ($song as $s, $co as $c, $ar as $a);
+            // (leave comment) make the song, composer, and artist append or overwrite the array and write to a file
+        //  $songlist = ("song"=>$song, "comp"=>$c, "art"=>$a)
+        //fwrite($outfile, $songlist . PHP_EOL)
     } 
     
+    private function readFromFile() {
+        if (file_exists($this->outfile)) {
+            $fileContents = file_get_contents($this->outfile);
+            if ($fileContents) {
+                return json_decode($fileContents, true);
+            }
+        }
+        return [];
+    }
 
+    private function writetoFile($data) {
+        file_put_contents($this->outfile, json_decode($data));
+    }
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $songName = $_POST["songName"];
+    $composer = $_POST["composer"];
+    $artist = $_POST["artist"];
+    
+    $songs = new Songs();
+    $songs->addSongToList($songName, $composer, $artist)
 }
 
-$songName = $_GET["songName"];
-$composer = $_GET["composer"];
-$artist = $_GET["artist"];
 ?>
--- surveyResults.php
+++ surveyResults.php
@@ -1,9 +1,42 @@
+<!--surveyResults.php-->
 <?php 
+class Songs {
+    private $outfile = "outfile.txt";
 
-for (i = 0; i)
+    function getSongList() {
+        if (file_exists($this->outfile)) {
+            $fileContents = file_get_contents($this->outfile);
+            if ($fileContents) {
+                return json_decode($fileContents, true);
+            }
+        }
+        return [];
+    }
+}
 
-echo "<p>The results from the survey are $"
-
-
-
+$songs = new Songs(); 
+$songList = $songs->getSongList();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Results from Favorite Songs Survey</title>
+</head>
+<body>
+    <h1>Results For Favorite Songs</h1>
+    <ul>
+        <?php foreach ($songList as $song): ?>
+            <li>
+                SongName: <?= htmlspecialchars($song["song"]) ?>
+                <ul>
+                    <li>SongName: <?= htmlspecialchars($song["composer"]) ?></li>
+                    <li>SongName: <?= htmlspecialchars($song["artist"]) ?></li>
+                </ul>    
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
+
